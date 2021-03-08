18cb4f734c5b73fc4e370bde22a6ce95ee90be7d
shin6142 <shin6142>
2021-03-08 12:29:15 +0900

    first commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+
+    def send_when_admin_reply(user) #メソッドに対して引数を設定
+        @user =  user
+        mail to: @user.email, subject: '【サイト名】 お問い合わせありがとうございます'
+      end
+
+end
